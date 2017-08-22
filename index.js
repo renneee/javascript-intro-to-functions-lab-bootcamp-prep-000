588f4cbefa58472791396e05445af737a4516e30
renneee <renpyang@gmail.com>
2017-08-22 08:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string === hello)
+  if (string === "hello")
     console.log(`${string.toLowerCase()}`)
     return "I can't hear you!"
 }
