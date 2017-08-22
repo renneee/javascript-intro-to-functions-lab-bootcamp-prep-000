74cbc1b21d7d306b5233d0fe79b60e369cc3e08c
renneee <renpyang@gmail.com>
2017-08-22 08:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function logShout (string) {
   console.log (`${string.toUpperCase()}`)
 }
 function logWhisper (string) {
-  console.log (`$(string.toLowerCase()}`)
+  console.log (`${string.toLowerCase()}`)
 }
