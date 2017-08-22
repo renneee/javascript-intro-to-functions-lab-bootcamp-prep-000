4222f1d7655cd61a847165ca48c4e3d6972bc394
renneee <renpyang@gmail.com>
2017-08-22 08:50:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ function sayHiToGrandma (string) {
   if (string === lowerCase)
     console.log(`${string}`)
     return "I can't hear you!"
-  }
-
-  else if (string === upperCase) {
+  } else if (string === upperCase) {
     console.log(`${string}`)
     return "YES INDEED!"
-  }
-
-  else (string === "I love you, Grandma.") {
+  } else (string === "I love you, Grandma.") {
     console.log(`${string}`)
     return "I love you, too."
   }
