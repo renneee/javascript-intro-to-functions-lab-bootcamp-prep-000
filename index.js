8419a5135ead7fcde725ac7d209d8cd3152475f0
renneee <renpyang@gmail.com>
2017-08-22 08:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ function whisper (string) {
   return string.toLowerCase()
 }
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
 function logShout (string) {
-  console.log (`${uppercase}`)
+  console.log (`${string.toUpperCase()}`)
 }
